42b453a775aad7aab327006316cdc9401ffeeb23
dagi <dagiteferi2011@gmail.com>
2025-09-30 03:40:01 +0300

    ccrate table

--- backend/src/api/models/models.py
+++ backend/src/api/models/models.py
@@ -1,6 +1,12 @@
-from pydantic import BaseModel
+from typing import Optional
+from sqlmodel import Field, SQLModel
 
-class Post(BaseModel):
+class SocialMediaAgent(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    description: Optional[str] = None
+
+class Post(SQLModel):
     id: str
     content: str
     approved: bool = False
